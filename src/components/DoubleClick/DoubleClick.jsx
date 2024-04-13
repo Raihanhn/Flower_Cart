7dbd227950a6c8b54c70f380bfd38eb7f5d22c58
raihan <raihanhn20@gmail.com>
2024-04-13 23:09:50 +0600

    button double click fnc completed

--- src/components/DoubleClick/DoubleClick.jsx
+++ src/components/DoubleClick/DoubleClick.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./doubleclick.css";
 
 const DoubleClick = () => {
-  return <div>DoubleClick</div>;
+  const [imageSrc, setImageSrc] = useState("/src/assets/lychee.jpg");
+
+  const handleDoubleClick = () => {
+    setImageSrc("/src/assets/mngo.jpg");
+  };
+
+  return (
+    <div className="btn">
+      <button className="button" onDoubleClick={handleDoubleClick}>
+        Double Click
+      </button>
+      <div className="imgdiv">
+        <img src={imageSrc} alt="Dynamic" />
+      </div>
+    </div>
+  );
 };
 
 export default DoubleClick;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FlowerDisplay from "../../components/FlowerDisplay/FlowerDisplay";
+import DoubleClick from "../../components/DoubleClick/DoubleClick";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <ExploreMenu category={category} setCategory={setCategory} />
       <FlowerDisplay category={category} />
+      <DoubleClick />
     </div>
   );
 };
